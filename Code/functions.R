556af6cdeb90176e2e4527fd36f6446ea389cdb5
Gascoigne <cgascoig@ic.ac.uk>
2023-05-11 08:53:50 +0100

    Predictions no longer include the iid term

--- Code/functions.R
+++ Code/functions.R
@@ -390,7 +390,7 @@ spline.real.fit <-
                     cohort_id = cohort %>% as.factor() %>% as.numeric(),
                     index_id = interaction(age, period) %>% as.numeric())
     
-    prediction <- predict(object = fit, newdata = dataPred2, type = 'link', se.fit = TRUE)
+    prediction <- predict(object = fit, newdata = dataPred2, type = 'link', se.fit = TRUE, exclude = 's(index_id)')
     
     results <-
       dataPred %>% 
@@ -491,6 +491,17 @@ randomWalk.real.fit <-
     }
     
     # model fit
+    # fit <- 
+    #   INLA::inla(formula, family = 'poisson', 
+    #              data = data2, offset = log(data2$N),
+    #              control.compute = control.compute,
+    #              control.predictor = list(compute = FALSE, link = 1),
+    #              control.inla = control.inla,
+    #              lincomb = NULL,
+    #              inla.mode = inla.mode,
+    #              verbose = verbose,
+    #              safe = TRUE,
+    #              ...)
     fit <- 
       INLA::inla(formula, family = 'poisson', 
                  data = data2, offset = log(data2$N),
@@ -500,14 +511,143 @@ randomWalk.real.fit <-
                  lincomb = NULL,
                  inla.mode = inla.mode,
                  verbose = verbose,
-                 safe = TRUE,
-                 ...)
+                 safe = TRUE)
+    
+    nSims <- 100
+    CI = 0.95; 
+    
+    # lower and upper CI calculators
+    lowerCI <- (1 - CI)/2
+    upperCI <- 1 - lowerCI
+    
+    allSamples <- INLA::inla.posterior.sample(n = nSims, result = fit, intern = TRUE, verbose = FALSE) # <--- remove selection from havard
+    
+    lineaPredFun <- function(newdata = NA, 
+                             mod = c('apc', 'ac', 'ap', 'pc', 'a', 'p', 'c')[1],
+                             slopeDrop = NULL){
+      
+      newdata <- newdata %>% as.data.frame()
+      
+      if (mod == 'apc'){
+        if(slopeDrop == 'a'){
+          result <-
+            # intercept
+            Intercept +
+            # linear terms
+            period_id * newdata$period_id +
+            cohort_id * newdata$cohort_id +
+            # random effects
+            age2_id[newdata$age2_id] +
+            period2_id[newdata$period2_id] +
+            cohort2_id[newdata$cohort2_id]
+        } else if (slopeDrop == 'p'){
+          result <-
+            Intercept +
+            # linear terms
+            age_id * newdata$age_id +
+            cohort_id * newdata$cohort_id +
+            # random effects
+            age2_id[newdata$age2_id] +
+            period2_id[newdata$period2_id] +
+            cohort2_id[newdata$cohort2_id]
+        } else if (slopeDrop == 'c'){
+          result <-
+            # intercept
+            Intercept +
+            # linear terms
+            age_id * newdata$age_id +
+            period_id * newdata$period_id +
+            # random effects
+            age2_id[newdata$age2_id] +
+            period2_id[newdata$period2_id] +
+            cohort2_id[newdata$cohort2_id]
+        }
+      } else if (mod == 'ac'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          age_id * newdata$age_id +
+          cohort_id * newdata$cohort_id +
+          # random effects
+          age2_id[newdata$age2_id] +
+          cohort2_id[newdata$cohort2_id]
+      } else if (mod == 'pc'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          period_id * newdata$period_id +
+          cohort_id * newdata$cohort_id +
+          # random effects
+          period2_id[newdata$period2_id] +
+          cohort2_id[newdata$cohort2_id]
+      } else if (mod == 'ap'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          age_id * newdata$age_id +
+          period_id * newdata$period_id +
+          # random effects
+          age2_id[newdata$age2_id] +
+          period2_id[newdata$period2_id]
+      } else if (mod == 'c'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          cohort_id * newdata$cohort_id +
+          # random effects
+          cohort2_id[newdata$cohort2_id]
+      } else if (mod == 'a'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          age_id * newdata$age_id +
+          # random effects
+          age2_id[newdata$age2_id]
+      } else if(mod == 'p'){
+        result <-
+          # intercept
+          Intercept +
+          # linear terms
+          period_id * newdata$period_id +
+          # random effects
+          period2_id[newdata$period2_id]
+      }
+      
+      return(result)
+      
+    }
+    
+    theta <- array(NA, c(nrow(data2), nSims))
+    for(i in 1:nrow(data2)){
+      theta[i,] <- INLA::inla.posterior.sample.eval(fun = lineaPredFun, samples = allSamples, mod = mod, slopeDrop = slopeDrop, newdata = data2[i,])
+    }
+    colnames(theta) <- paste0('theta:', 1:nSims)
+    theta2 <- cbind(data2, theta)
+    
+    
+    my.summary <- function(x, lowerCI, upperCI) {
+      
+      # x = theta[,1]; lowerCI = lowerCI; upperCI = upperCI
+      
+      qntl <- quantile(x, probs = c(lowerCI, 0.5, upperCI))
+      data.frame(mean = mean(x), variance = var(x), lower = qntl[1], median = qntl[2], upper = qntl[3])
+      
+    }
+    
     
     results <-
-      data %>% 
-      dplyr::mutate(yHat = fit$summary.linear.predictor$`0.5quant`,
-                    lower = fit$summary.linear.predictor$`0.025quant`,
-                    upper = fit$summary.linear.predictor$`0.975quant`)
+      theta2 %>% 
+      dplyr::mutate(dplyr::select(., starts_with('theta:')) %>%
+                      apply(., 1, my.summary, lowerCI = lowerCI, upperCI = upperCI) %>% 
+                      lapply(., data.frame) %>%
+                      do.call(rbind, .),
+                    yHat = median) %>%
+      dplyr::select(age, period, cohort, N, y, yHat, lower, upper)
     
     return(results)
   }
@@ -939,3 +1079,207 @@ get.sensitivity.final.plot <- function(simRun, ...){
   return(list(p = p, simPlots = simPlots, individualEffectPlots = individualEffectPlots))
   
 }
+
+
+# # OLD:::: real data ----
+# 
+# ## spline model ----
+# 
+# spline.real.fit <- 
+#   function(dataEst, dataPred, mod = c('apc','ac','ap','pc','a','p','c')[1],
+#            bs = NULL, knots = NULL, fixed = NULL, slopeDrop = NULL){
+#     
+#     # # inputs to run all parts of the function
+#     # age = 10:99; period = 2000:2020; N = 25;
+#     # M = 5;
+#     # data = data.sim(age = age, period = period, N = N, M = M,
+#     #                 FUNage = age.fun,  FUNperiod = per.fun, FUNcohort = coh.fun)
+#     # mod = 'apc'; slopeDrop = 'c'; bs = 'cr'
+#     # knots = list(age = 5, period = 5, cohort = 8)
+#     # fixed = list(age = F,period = F,cohort = F)
+#     
+#     if(is.null(knots)|!is.null(knots) & !is.list(knots)) stop('Warning: Need a list for number of knots for each effect labelled age, period, cohort.')
+#     if(is.null(bs)) stop('Warning: Need a basis function.')
+#     if(is.null(fixed)|!is.null(fixed) & !is.list(fixed)) stop('Warning: Need a list for whether an effect is penalised or not.')
+#     if(mod == 'apc'&&is.null(slopeDrop)) stop('Warning: When fitting an APC model need to drop linear column of one of age (a), period (p) or cohort (c).')
+#     if(mod == 'apc'&&!(slopeDrop %in% c('a', 'p', 'c'))) stop('slopeDrop needs to be one of a, p or c')
+#     
+#     # if data needs additional changes
+#     ## center data
+#     dataEst2 <- 
+#       dataEst %>%
+#       dplyr::mutate(age_id = age %>% as.factor() %>% as.numeric(),
+#                     period_id = period %>% as.factor() %>% as.numeric(),
+#                     cohort_id = cohort %>% as.factor() %>% as.numeric(),
+#                     index_id = interaction(age, period) %>% as.numeric())
+#     
+#     ak <- knots$age; pk <- knots$period; ck <- knots$cohort
+#     afx <- fixed$age; pfx <- fixed$period; cfx <- fixed$cohort # default is to have fixed = FALSE for penalisation
+#     
+#     formula <- 
+#       as.formula(y ~  
+#                    age_id + period_id + cohort_id + 
+#                    s(age_id, bs = bs, k = ak, fx = afx) + 
+#                    s(period_id, bs = bs, k = pk, fx = pfx) + 
+#                    s(cohort_id, bs = bs, k = ck, fx = cfx) +
+#                    s(index_id, bs = 're'))
+#     # update pre and post forumla for the temporal models
+#     # # do not remove any linear terms from pre fit as they are all needed later in 
+#     # # re-parameterisation
+#     if (mod == 'apc'){
+#       if(slopeDrop == 'a'){
+#         formula <- update(formula, ~. - age_id)
+#       } else if (slopeDrop == 'p'){
+#         formula <- update(formula, ~. - period_id)
+#       } else if (slopeDrop == 'c'){
+#         formula <- update(formula, ~. - cohort_id)
+#       }
+#     } else if (mod == 'ac'){
+#       formula <- update(formula, ~.- period_id - s(period_id, bs = bs, k = pk, fx = pfx))
+#     } else if (mod == 'pc'){
+#       formula <- update(formula, ~. - age_id - s(age_id, bs = bs, k = ak, fx = afx))
+#     } else if (mod == 'ap'){
+#       formula <- update(formula, ~. - cohort_id - s(cohort_id, bs = bs, k = ck, fx = cfx))
+#     } else if (mod == 'c'){
+#       formula <- update(formula, ~.- age_id - s(age_id, bs = bs, k = ak, fx = afx) - period_id - s(period_id, bs = bs, k = pk, fx = pfx))
+#     } else if (mod == 'a'){
+#       formula <- update(formula, ~.- period_id - s(period_id, bs = bs, k = pk, fx = pfx) - cohort_id - s(cohort_id, bs = bs, k = ck, fx = cfx))
+#     } else if (mod == 'p'){
+#       formula <- update(formula, ~.- age_id - s(age_id, bs = bs, k = ak, fx = afx) - cohort_id - s(cohort_id, bs = bs, k = ck, fx = cfx))
+#     }
+#     
+#     # use all cores avaliable
+#     ctrl <- list(nthreads = parallel::detectCores())
+#     
+#     # model fit
+#     fit <- mgcv::gam(formula, offset = log(N), family = 'poisson', data = dataEst2, method = 'REML', control = ctrl)
+#     
+#     # need to match the naming convention in spline.fit()
+#     dataPred2 <- 
+#       dataPred %>% 
+#       dplyr::mutate(age_id = age %>% as.factor() %>% as.numeric(),
+#                     period_id = period %>% as.factor() %>% as.numeric(),
+#                     cohort_id = cohort %>% as.factor() %>% as.numeric(),
+#                     index_id = interaction(age, period) %>% as.numeric())
+#     
+#     prediction <- predict(object = fit, newdata = dataPred2, type = 'link', se.fit = TRUE)
+#     
+#     results <-
+#       dataPred %>% 
+#       dplyr::mutate(yHat = prediction$fit %>% as.vector(),
+#                     se = prediction$se.fit %>% as.vector()) %>% 
+#       dplyr::rowwise() %>% 
+#       dplyr::mutate(lower = yHat + qnorm(p = 0.025, mean = 0, sd = 1) * se,
+#                     upper = yHat + qnorm(p = 0.975, mean = 0, sd = 1) * se) %>% 
+#       dplyr::ungroup()
+#     
+#     return(results)
+#     
+#   }
+# 
+# ## random walk model ----
+# 
+# randomWalk.real.fit <-
+#   function(data, mod = c('apc', 'ac', 'ap', 'pc', 'a', 'p', 'c')[1],
+#            randomWalk = c('rw1', 'rw2')[2],
+#            slopeDrop = NULL, 
+#            pc.u = 1, pc.alpha = 0.01,
+#            control.inla = list(strategy = 'adaptive', int.strategy = 'auto'),
+#            inla.mode = c('classic', 'twostage', 'experimental')[3],
+#            control.compute = list(config = TRUE), verbose = FALSE, ...){
+#     
+#     
+#     # # inputs to run all parts of the function
+#     # data = alcoholData.randomWalkPredict
+#     # mod = 'apc'; slopeDrop = 'c'; randomWalk = 'rw1';
+#     # pc.u = 1; pc.alpha = 0.01;
+#     # control.inla = list(strategy = 'adaptive', int.strategy = 'auto');
+#     # inla.mode = c('classic', 'twostage', 'experimental')[3];
+#     # control.compute = list(config = TRUE); verbose = FALSE
+#     
+#     if (!"config" %in% names(control.compute)) {
+#       message("config = TRUE is added to control.compute so that posterior draws can be taken.")
+#       control.compute$config <- TRUE
+#     }
+#     if(mod == 'apc' && is.null(slopeDrop)) stop('Warning: When fitting an APC model need to drop linear column of one of age, period or cohort.')
+#     if(mod == 'apc' &&! (slopeDrop %in% c('a', 'p', 'c'))) stop('slopeDrop needs to be one of a, p or c')
+#     
+#     # data augmentation
+#     data2 <- 
+#       data %>%
+#       dplyr::mutate(age_id = age %>% as.factor() %>% as.numeric(),
+#                     period_id = period %>% as.factor() %>% as.numeric(),
+#                     cohort_id = cohort %>% as.factor() %>% as.numeric(),
+#                     age2_id = age_id,
+#                     period2_id = period_id,
+#                     cohort2_id = cohort_id,
+#                     index_id = interaction(age, period) %>% as.numeric())
+#     
+#     # define the formula for INLA
+#     # pc hyper priors
+#     ## temproal
+#     hyper_pc_time <- list(prec = list(prior = 'pc.prec', param = c(pc.u, pc.alpha)))
+#     
+#     # full temporal formula
+#     formula <- 
+#       y ~ age_id + period_id + cohort_id +
+#       f(age2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) +
+#       f(period2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) +
+#       f(cohort2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) +
+#       f(index_id, model = 'iid', hyper = hyper_pc_time)
+#     
+#     # update pre and post forumla for the temporal models
+#     # # do not remove any linear terms from pre fit as they are all needed later in 
+#     # # re-parameterisation
+#     if (mod == 'apc'){
+#       if(slopeDrop == 'a'){
+#         formula <- update(formula, ~. - age_id)
+#       } else if (slopeDrop == 'p'){
+#         formula <- update(formula, ~. - period_id)
+#       } else if (slopeDrop == 'c'){
+#         formula <- update(formula, ~. - cohort_id)
+#       }
+#     } else if (mod == 'ac'){
+#       formula <- update(formula, ~.
+#                         - period_id - f(period2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     } else if (mod == 'pc'){
+#       formula <- update(formula, ~.
+#                         - age_id - f(age2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     } else if (mod == 'ap'){
+#       formula <- update(formula, ~.
+#                         - cohort_id - f(cohort2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     } else if (mod == 'c'){
+#       formula <- update(formula, ~.
+#                         - age_id - f(age2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) -
+#                           period_id - f(period2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     } else if (mod == 'a'){
+#       formula <- update(formula, ~.
+#                         - period_id - f(period2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) -
+#                           cohort_id - f(cohort2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     } else if(mod == 'p'){
+#       formula <- update(formula, ~.
+#                         - age_id - f(age2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE) -
+#                           cohort_id - f(cohort2_id, model = randomWalk, hyper = hyper_pc_time, constr = TRUE))
+#     }
+#     
+#     # model fit
+#     fit <- 
+#       INLA::inla(formula, family = 'poisson', 
+#                  data = data2, offset = log(data2$N),
+#                  control.compute = control.compute,
+#                  control.predictor = list(compute = FALSE, link = 1),
+#                  control.inla = control.inla,
+#                  lincomb = NULL,
+#                  inla.mode = inla.mode,
+#                  verbose = verbose,
+#                  safe = TRUE,
+#                  ...)
+#     
+#     results <-
+#       data %>% 
+#       dplyr::mutate(yHat = fit$summary.linear.predictor$`0.5quant`,
+#                     lower = fit$summary.linear.predictor$`0.025quant`,
+#                     upper = fit$summary.linear.predictor$`0.975quant`)
+#     
+#     return(results)
+#   }
