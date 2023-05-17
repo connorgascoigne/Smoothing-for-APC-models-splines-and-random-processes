5228817862e26ebd491b71fcec4396c7eafd6dae
Gascoigne <cgascoig@ic.ac.uk>
2023-05-17 13:11:37 +0100

    include U = 6 PC prior

--- Code/Model Fitting/pcPriorsDifference.R
+++ Code/Model Fitting/pcPriorsDifference.R
@@ -41,26 +41,36 @@ allPrec <- seq(from = 0.05, to = 1000, length.out = n) # precisions
 allSD <- 1/sqrt(allPrec)
 
 results <- 
-  data.frame(allPrec = rep(allPrec, times = 2),
-             allSD = rep(allSD, times = 2),
+  data.frame(allPrec = rep(allPrec, times = 3),
+             allSD = rep(allSD, times = 3),
              prec = c(INLA::inla.pc.dprec(prec = allPrec, u = 1, alpha = 0.01), 
-                      INLA::inla.pc.dprec(prec = allPrec, u = 3, alpha = 0.01)),
+                      INLA::inla.pc.dprec(prec = allPrec, u = 3, alpha = 0.01), 
+                      INLA::inla.pc.dprec(prec = allPrec, u = 6, alpha = 0.01)),
              sd = c(dexp(allSD, rate = -log(0.01)/1), 
-                    dexp(allSD, rate = -log(0.01)/3)),
+                    dexp(allSD, rate = -log(0.01)/3), 
+                    dexp(allSD, rate = -log(0.01)/6)),
              U = c(rep(1, times = n),
-                       rep(3, times = n)) %>% 
-               factor(., levels = c(1, 3), labels = c(1, 3))) %>% 
+                   rep(3, times = n),
+                   rep(6, times = n)) %>% 
+               factor(., levels = c(1, 3, 6), labels = c('U = 1', 'U = 3', 'U = 6'))) %>% 
   tibble::as_tibble()
 
 pcPriorsPlot <- 
-  ggplot2::ggplot(data = results, aes(x = allSD, y = sd, group = U, colour = U)) +
+  ggplot2::ggplot(data = results, aes(x = allSD, y = sd, group = U, colour = U, linetype = U)) +
   ggplot2::geom_line() +
   ggplot2::labs(x = 'Standard Deviation', y = '') +
-  ggplot2::scale_color_manual(labels = c(latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 1$)'), 
-                                           latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 3$)')),
-                                values = c('red3', 'blue3')) +
-  my.theme(legend.title = element_blank(),
-           legend.position = 'top',
+  ggplot2::scale_color_manual(values = c('red3', 'blue3', 'green4'),
+                              labels = 
+                                c(latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 1$)'), 
+                                  latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 3$)'), 
+                                  latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 6$)'))) + 
+  ggplot2::scale_linetype_manual(values = c('solid', 'dashed', 'dotted'),
+                                 labels = 
+                                   c(latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 1$)'), 
+                                     latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 3$)'), 
+                                     latex2exp::TeX(r'($\alpha = 0.01 \ and \ U = 6$)'))) + 
+  my.theme(legend.position = c(0.6, 0.8),
+           legend.title = element_blank(),
            text = element_text(size = textSize)); pcPriorsPlot
 
 ggplot2::ggsave(filename = paste0(resultsDir, '/pcPriorsPlot.png'),
