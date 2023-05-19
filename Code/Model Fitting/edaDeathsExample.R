d772cb320e4ebabb00d2b9c0ae464f74cc73376a
Gascoigne <cgascoig@ic.ac.uk>
2023-05-19 15:26:56 +0100

    full removal of rw1 model

--- Code/Model Fitting/edaDeathsExample.R
+++ Code/Model Fitting/edaDeathsExample.R
@@ -137,15 +137,6 @@ splineFit <-
 
 #### random walk ----
 
-# rw1Fit <- 
-#   randomWalk.fit(data = alcoholData,
-#                  predictFrom = 2018,
-#                  mod = 'apc', slopeDrop = 'c', randomWalk = 'rw1',
-#                  pc.u = 1, pc.alpha = 0.01,
-#                  control.inla = list(strategy = 'adaptive', int.strategy = 'auto'),
-#                  inla.mode = c('classic', 'twostage', 'experimental')[3],
-#                  control.compute = list(config = TRUE), verbose = FALSE)
-
 rw2Fit <- 
   randomWalk.fit(data = alcoholData,
                  predictFrom = 2018,
@@ -157,13 +148,6 @@ rw2Fit <-
 
 
 ### results collecting ----
-
-# alcoholResults <-
-#   rbind(splineFit %>% dplyr::select(-se) %>%  dplyr::mutate(model = 'Spline'),
-#         rw1Fit %>% dplyr::select(-y, -N) %>% dplyr::mutate(model = 'RW1'),
-#         rw2Fit %>% dplyr::select(-y, -N) %>% dplyr::mutate(model = 'RW2')) %>%
-#   dplyr::mutate(model = model %>% factor(., levels = c('Spline', 'RW1', 'RW2'))) %>%
-#   dplyr::left_join(., alcoholData %>% dplyr::select(Age_Group, age) %>% dplyr::distinct(), by = 'age')
 
 alcoholResults <-
   rbind(splineFit %>% dplyr::select(-se) %>%  dplyr::mutate(model = 'Spline'),
@@ -187,17 +171,6 @@ alcoholPredictedHeatmap_spline <-
   my.theme(text = element_text(size = textSize),
            axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)); alcoholPredictedHeatmap_spline
 
-# alcoholPredictedHeatmap_rw1 <-
-#   ggplot2::ggplot(alcoholResults %>% dplyr::filter(model == 'RW1'), aes(y = age, x = period, fill = yHat)) + 
-#   ggplot2::scale_x_continuous(breaks = seq(2005, 2021, 1)) +
-#   ggplot2::scale_y_continuous(breaks = seq(25, 85, 5)) +
-#   ggplot2::geom_raster() +
-#   ggplot2::scale_fill_viridis_c('Log rate', option = 'G', direction = 1) +
-#   ggplot2::geom_vline(aes(xintercept = max(period)-3), color = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::labs(x = 'Year', y = 'Age') +
-#   my.theme(text = element_text(size = textSize),
-#            axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)); alcoholPredictedHeatmap_rw1
-
 alcoholPredictedHeatmap_rw2 <-
   ggplot2::ggplot(alcoholResults %>% dplyr::filter(model == 'RW2'), aes(y = age, x = period, fill = yHat)) + 
   ggplot2::scale_x_continuous(breaks = seq(2005, 2021, 1)) +
@@ -212,24 +185,12 @@ alcoholPredictedHeatmap_rw2 <-
 ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredictedHeatmap_spline.png'),
                 plot = alcoholPredictedHeatmap_spline,
                 height = height, width = width)
-# ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredictedHeatmap_rw1.png'),
-#                 plot = alcoholPredictedHeatmap_rw1,
-#                 height = height, width = width)
 ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredictedHeatmap_rw2.png'),
                 plot = alcoholPredictedHeatmap_rw2,
                 height = height, width = width)
 
 
 #### direct comp ----
-
-# alcoholPredicted_splineVsRW1 <-
-#   ggplot2::ggplot(alcoholResults %>% 
-#                     tidyr::pivot_wider(., names_from = model, values_from = c('yHat', 'lower', 'upper')), 
-#                   aes(x = yHat_Spline, y = yHat_RW1)) +
-#   ggplot2::geom_abline(aes(intercept = 0, slope = 1), colour = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::geom_point() +
-#   ggplot2::labs(x = 'Spline', y = 'RW1') +
-#   my.theme(); alcoholPredicted_splineVsRW1
 
 alcoholPredicted_splineVsRW2 <-
   ggplot2::ggplot(alcoholResults %>% 
@@ -240,25 +201,9 @@ alcoholPredicted_splineVsRW2 <-
   ggplot2::labs(x = 'Spline', y = 'RW2') +
   my.theme(); alcoholPredicted_splineVsRW2
 
-# alcoholPredicted_RW2VsRW1 <-
-#   ggplot2::ggplot(alcoholResults %>% 
-#                     tidyr::pivot_wider(., names_from = model, values_from = c('yHat', 'lower', 'upper')),
-#                   aes(x = yHat_RW2, y = yHat_RW1)) +
-#   ggplot2::geom_abline(aes(intercept = 0, slope = 1), colour = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::geom_point() +
-#   ggplot2::labs(x = 'RW2', y = 'RW1') +
-#   my.theme(); alcoholPredicted_RW2VsRW1
-
-
-# ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredicted_splineVsRW1.png'),
-#                 plot = alcoholPredicted_splineVsRW1,
-#                 height = height, width = width)
 ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredicted_splineVsRW2.png'),
                 plot = alcoholPredicted_splineVsRW2,
                 height = height, width = width)
-# ggplot2::ggsave(filename = paste0(resultsDir, '/alcoholPredicted_RW2VsRW1.png'),
-#                 plot = alcoholPredicted_RW2VsRW1,
-#                 height = height, width = width)
 
 #### line plots ----
 
@@ -314,15 +259,6 @@ splineFit <-
 
 #### random walk ----
 
-# rw1Fit <- 
-#   randomWalk.fit(data = selfHarmData,
-#                  predictFrom = 2018,
-#                  mod = 'apc', slopeDrop = 'c', randomWalk = 'rw1',
-#                  pc.u = 1, pc.alpha = 0.01,
-#                  control.inla = list(strategy = 'adaptive', int.strategy = 'auto'),
-#                  inla.mode = c('classic', 'twostage', 'experimental')[3],
-#                  control.compute = list(config = TRUE), verbose = FALSE)
-
 rw2Fit <- 
   randomWalk.fit(data = selfHarmData,
                  predictFrom = 2018,
@@ -334,13 +270,6 @@ rw2Fit <-
 
 
 ### results collecting ----
-
-# selfHarmResults <-
-#   rbind(splineFit %>% dplyr::select(-se) %>%  dplyr::mutate(model = 'Spline'),
-#         rw1Fit %>% dplyr::select(-y, -N) %>% dplyr::mutate(model = 'RW1'),
-#         rw2Fit %>% dplyr::select(-y, -N) %>% dplyr::mutate(model = 'RW2')) %>%
-#   dplyr::mutate(model = model %>% factor(., levels = c('Spline', 'RW1', 'RW2'))) %>%
-#   dplyr::left_join(., selfHarmDataAll %>% dplyr::select(Age_Group, age) %>% dplyr::distinct(), by = 'age')
 
 selfHarmResults <-
   rbind(splineFit %>% dplyr::select(-se) %>%  dplyr::mutate(model = 'Spline'),
@@ -363,17 +292,6 @@ selfHarmPredictedHeatmap_spline <-
   my.theme(text = element_text(size = textSize),
            axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)); selfHarmPredictedHeatmap_spline
 
-# selfHarmPredictedHeatmap_rw1 <-
-#   ggplot2::ggplot(selfHarmResults %>% dplyr::filter(model == 'RW1'), aes(y = age, x = period, fill = yHat)) + 
-#   ggplot2::scale_x_continuous(breaks = seq(2005, 2021, 1)) +
-#   ggplot2::scale_y_continuous(breaks = seq(25, 85, 5)) +
-#   ggplot2::geom_raster() +
-#   ggplot2::scale_fill_viridis_c('Log rate', option = 'G', direction = 1) +
-#   ggplot2::geom_vline(aes(xintercept = max(period)-3), color = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::labs(x = 'Year', y = 'Age') +
-#   my.theme(text = element_text(size = textSize),
-#            axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)); selfHarmPredictedHeatmap_rw1
-
 selfHarmPredictedHeatmap_rw2 <-
   ggplot2::ggplot(selfHarmResults %>% dplyr::filter(model == 'RW2'), aes(y = age, x = period, fill = yHat)) + 
   ggplot2::scale_x_continuous(breaks = seq(2005, 2021, 1)) +
@@ -388,24 +306,12 @@ selfHarmPredictedHeatmap_rw2 <-
 ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredictedHeatmap_spline.png'),
                 plot = selfHarmPredictedHeatmap_spline,
                 height = height, width = width)
-# ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredictedHeatmap_rw1.png'),
-#                 plot = selfHarmPredictedHeatmap_rw1,
-#                 height = height, width = width)
 ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredictedHeatmap_rw2.png'),
                 plot = selfHarmPredictedHeatmap_rw2,
                 height = height, width = width)
 
 
 #### direct comp ----
-
-# selfHarmPredicted_splineVsRW1 <-
-#   ggplot2::ggplot(selfHarmResults %>% 
-#                     tidyr::pivot_wider(., names_from = model, values_from = c('yHat', 'lower', 'upper')), 
-#                   aes(x = yHat_Spline, y = yHat_RW1)) +
-#   ggplot2::geom_abline(aes(intercept = 0, slope = 1), colour = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::geom_point() +
-#   ggplot2::labs(x = 'Spline', y = 'RW1') +
-#   my.theme(); selfHarmPredicted_splineVsRW1
 
 selfHarmPredicted_splineVsRW2 <-
   ggplot2::ggplot(selfHarmResults %>% 
@@ -416,25 +322,9 @@ selfHarmPredicted_splineVsRW2 <-
   ggplot2::labs(x = 'Spline', y = 'RW2') +
   my.theme(); selfHarmPredicted_splineVsRW2
 
-# selfHarmPredicted_RW2VsRW1 <-
-#   ggplot2::ggplot(selfHarmResults %>% 
-#                     tidyr::pivot_wider(., names_from = model, values_from = c('yHat', 'lower', 'upper')),
-#                   aes(x = yHat_RW2, y = yHat_RW1)) +
-#   ggplot2::geom_abline(aes(intercept = 0, slope = 1), colour = 'red3', linetype = 'dotted', linewidth = 1) +
-#   ggplot2::geom_point() +
-#   ggplot2::labs(x = 'RW2', y = 'RW1') +
-#   my.theme(); selfHarmPredicted_RW2VsRW1
-
-
-# ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredicted_splineVsRW1.png'),
-#                 plot = selfHarmPredicted_splineVsRW1,
-#                 height = height, width = width)
 ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredicted_splineVsRW2.png'),
                 plot = selfHarmPredicted_splineVsRW2,
                 height = height, width = width)
-# ggplot2::ggsave(filename = paste0(resultsDir, '/selfHarmPredicted_RW2VsRW1.png'),
-#                 plot = selfHarmPredicted_RW2VsRW1,
-#                 height = height, width = width)
 
 #### line plots ----
 
